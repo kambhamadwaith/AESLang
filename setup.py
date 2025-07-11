838597d0990ded5513654935f091f0fb2f062a46
kali <kali@kali>
2025-07-11 10:32:31 +0530

    Added PyPI-ready setup.py for AESLang v1.1.0

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='aeslang',
-    version='1.0.0',
+    name="aeslang",
+    version="1.1.0",
+    author="Kambham Adwaith",
+    author_email="kambhamadwaith@algorithmeduspace.com",  # 🔁 Replace with your actual email
+    description="A programming language made entirely of the word AES",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kambhamadwaith/AESLang",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Interpreters",
+        "Intended Audience :: Developers",
+    ],
+    python_requires=">=3.6",
     entry_points={
-        'console_scripts': ['aeslang=aeslang.main:main']
+        "console_scripts": [
+            "aeslang=aeslang.main:main"
+        ]
     },
-    python_requires='>=3.6',
+    include_package_data=True,
 )
